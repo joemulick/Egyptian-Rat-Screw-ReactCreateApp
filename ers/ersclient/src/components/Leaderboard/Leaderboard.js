0b2cb7f8ef27fcad7301439c4bbfe9a97389c038
Joe Mulick <joemulick@gmail.com>
2017-12-22 13:28:15 -0800

    added react router 'BrowserRouter'

---
+++ ers/ersclient/src/components/Leaderboard/Leaderboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Leaderboard extends Component {
+  render() {
+    return (  
+
+    	<div>
+    		<p>This is the Leaderboard page</p>
+    	</div>  	
+    	
+    );
+  }
+}
+
+export default Leaderboard;
